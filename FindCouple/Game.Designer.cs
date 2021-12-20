c7987e8a041d74b95435be105d9802bf9343304b
Fidan-android <gum.fidan2017@yandex.ru>
2021-12-20 19:18:18 +0300

    The end

--- FindCouple/Game.Designer.cs
+++ FindCouple/Game.Designer.cs
@@ -28,24 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // panel1
             // 
-            this.button1.Location = new System.Drawing.Point(102, 230);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(391, 135);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(700, 70);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(581, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "5:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(235, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Открытых: 0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 702);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 36);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ходов: 0";
             // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(680, 657);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(700, 747);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Palatino Linotype", 14.25F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Margin = new System.Windows.Forms.Padding(6);
@@ -55,12 +103,20 @@
             this.Text = "Game";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Game_FormClosing);
             this.Load += new System.EventHandler(this.Game_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- FindCouple/Game.cs
+++ FindCouple/Game.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -22,21 +24,6 @@ namespace FindCouple
             this.N = n;
             this.M = m;
             this.forma = form;
-            GenerateOfColors();
-
-            this.Size = new Size(N * 70 + 70, M * 70 + 70);
-
-        }
-
-        private void GenerateOfColors()
-        {
-            Random random = new Random();
-            for (int i = 0; i < N * M / 2; i++)
-            {
-                Color temp = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
-                colors.Add(temp);
-                colors.Add(temp);
-            }
         }
 
         private void Game_FormClosing(object sender, FormClosingEventArgs e)
@@ -44,21 +31,36 @@ namespace FindCouple
             this.forma.Show();
         }
 
+        List<PictureBox> pictureBoxes = new List<PictureBox>();
         private void Game_Load(object sender, EventArgs e)
         {
-            int y = 10, index = 0;
+            GenerateOfColors();
+            this.Size = new Size(N * 70 + 140, M * 70 + 210);
+            createGameOptions();
+            int y = 70, index = 0;
             for (int i = 0; i < M; i++)
             {
-                int x = 25;
+                int x = 70;
                 for (int j = 0; j < N; j++)
                 {
+                    for (int k = 0; k < N; k++)
+                    {
+                        Random random = new Random();
+                        index = random.Next(0, colors.Count);
+                    }
+                    Console.WriteLine(index);
+
                     PictureBox picture = new PictureBox()
                     {
                         Size = new Size(70, 70),
                         Location = new Point(x, y),
                         BackColor = colors[index],
+
                         BorderStyle = BorderStyle.FixedSingle
                     };
+
+                    colors.RemoveAt(index);
+                    pictureBoxes.Add(picture);
 
                     PictureBox whitePicture = new PictureBox()
                     {
@@ -67,13 +69,155 @@ namespace FindCouple
                         BackColor = Color.White,
                         BorderStyle = BorderStyle.FixedSingle
                     };
-                    //Controls.Add(whitePicture);
+                    whitePicture.Click += whiteClick;
+                    Controls.Add(whitePicture);
                     Controls.Add(picture);
                     x += 70;
-                    index++;
                 }
                 y += 70;
             }
+
+            timer2.Start();
+        }
+
+        private void GenerateOfColors()
+        {
+            Random random = new Random();
+            for (int i = 0; i < N * M / 2; i++)
+            {
+                Color temp = Color.White;
+                for (int k = 0; k < N * M; k++)
+                {
+                    temp = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+                }
+                colors.Add(temp);
+                colors.Add(temp);
+            }
+        }
+
+        private void createGameOptions()
+        {
+            switch(N)
+            {
+                case 2: case 3: case 4: {
+                        label1.Font = new Font("Palatino Linotype", 18);
+                        label2.Font = new Font("Palatino Linotype", 18);
+                        label1.Location = new Point(N * 70 + (70 - label1.Width), 0);
+                        label2.Location = new Point(0, 0);
+                        break;
+                    }
+                case 5: case 6: case 7: {
+                        label1.Font = new Font("Palatino Linotype", 20);
+                        label2.Font = new Font("Palatino Linotype", 20);
+                        label1.Location = new Point(N * 70 + (70 - label1.Width), 0);
+                        label2.Location = new Point(0, 0);
+                        break;
+                    }
+                case 8: case 9: {
+                        label1.Font = new Font("Palatino Linotype", 24);
+                        label2.Font = new Font("Palatino Linotype", 24);
+                        label1.Location = new Point(N * 70 + (70 - label1.Width), 0);
+                        label2.Location = new Point(0, 0);
+                        break;
+                    }
+            }
+
+            label3.Font = new Font("Palatino Linotype", 18);
+            label3.Location = new Point(0, M * 70 + (70 + label3.Height));
+        }
+
+        int numberOfBlocksShown = 0;
+        PictureBox savedPictureBox = new PictureBox();
+        List<PictureBox> lastWhiteBoxes = new List<PictureBox>();
+        private int timeOut = 300, numberOfOpenColors = 0, numberOfSteps = 0;
+        private bool isAllowedPress = true;
+
+        public void whiteClick(object sender, EventArgs e)
+        {
+            if (numberOfBlocksShown < 2 && isAllowedPress)
+            {
+                numberOfSteps++;
+                label3.Text = "Ходов: " + numberOfSteps.ToString();
+
+                PictureBox tempBox = (PictureBox)sender;
+
+                tempBox.Visible = false;
+                numberOfBlocksShown++;
+
+
+                if (numberOfBlocksShown == 2)
+                {
+                    foreach (PictureBox pb in pictureBoxes)
+                    {
+                        if (tempBox.Location == pb.Location)
+                        {
+                            if (savedPictureBox.BackColor == pb.BackColor)
+                            {
+                                numberOfBlocksShown -= 2;
+                                lastWhiteBoxes.Clear();
+                                numberOfOpenColors++;
+                                label2.Text = "Открытых: " + numberOfOpenColors.ToString();
+                            }
+                            else
+                            {
+                                lastWhiteBoxes.Add(tempBox);
+                                timer1.Start();
+                            }
+                        }
+                    }
+                    return;
+                }
+
+                foreach (PictureBox pb in pictureBoxes)
+                {
+                    if (tempBox.Location == pb.Location)
+                    {
+                        savedPictureBox = pb;
+                    }
+                }
+
+                lastWhiteBoxes.Add(tempBox);
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timeOut -= 1;
+            TimeSpan ts = TimeSpan.FromSeconds(timeOut);
+            label1.Text = string.Format("{0}:{1}", ts.Minutes, ts.Seconds); ;
+            if (timeOut == 0)
+            {
+                isAllowedPress = !isAllowedPress;
+                timer2.Stop();
+                if (M*N /2 != numberOfOpenColors)
+                {
+                    MessageBox.Show("Время истекло, к сожалению вы не смогли открыть все цвета=(");
+                    this.Close();
+                }
+            }
+
+            if (M * N / 2 == numberOfOpenColors)
+            {
+                timer2.Stop();
+                MessageBox.Show("Поздравляем, вы выиграли!!");
+                this.Close();
+            }
+        }
+
+        private void StartNewGame(object sender, EventArgs e)
+        {
+            new Game(N + 1, M + 1, forma).ShowDialog();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (PictureBox pb in lastWhiteBoxes)
+            {
+                pb.Visible = true;
+            }
+
+            numberOfBlocksShown -= 2;
+            timer1.Stop();
         }
     }
 }
